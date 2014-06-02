338e2021f3fa272a156728ca649ab720d03f79d7
Hayssam Hajar <hayssam.hajar@gmail.com>
2014-06-01 17:13:53 -0700

    lesson 2 - kalman matrices quiz

--- lesson2-kalman_matrices.py
+++ lesson2-kalman_matrices.py
@@ -146,9 +146,18 @@ def filter(x, P):
     for n in range(len(measurements)):
 
         # measurement update
+        y = matrix([[measurements[n]]]) - H * x
+        S = H * P * matrix.transpose(H) + R
+        K = P * matrix.transpose(H) * matrix.inverse(S)
+        x = x + K * y
+        P = (I - K * H) * P
 
         # prediction
+        x = F * x + u
+        P = F * P * matrix.transpose(F)
 
+        # don't change these print/show statements here---
+        # they are for the grader to check your answer
         print 'x= '
         x.show()
         print 'P= '
@@ -160,12 +169,16 @@ def filter(x, P):
 
 measurements = [1, 2, 3]
 
-x = matrix([[0.], [0.]]) # initial state (location and velocity)
-P = matrix([[1000., 0.], [0., 1000.]]) # initial uncertainty
-u = matrix([[0.], [0.]]) # external motion
-F = matrix([[1., 1.], [0, 1.]]) # next state function
-H = matrix([[1., 0.]]) # measurement function
-R = matrix([[1.]]) # measurement uncertainty
-I = matrix([[1., 0.], [0., 1.]]) # identity matrix
+
+
+x = matrix([[0.], [0.]])  # initial state (location and velocity)
+P = matrix([[1000., 0.], [0., 1000.]])  # initial uncertainty
+u = matrix([[0.], [0.]])  # external motion
+F = matrix([[1., 1.], [0, 1.]])  # next state function
+H = matrix([[1., 0.]])  # measurement function
+R = matrix([[1.]])  # measurement uncertainty
+I = matrix([[1., 0.], [0., 1.]])  # identity matrix
 
 filter(x, P)
+
+print P
