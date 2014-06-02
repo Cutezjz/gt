46177817840e8a17bf44cd3a6d64c14555bc8ebf
Hayssam Hajar <hayssam.hajar@gmail.com>
2014-06-01 18:43:41 -0700

    fixed spacing for comments

--- lesson2-kalman_matrices.py
+++ lesson2-kalman_matrices.py
@@ -160,7 +160,7 @@ def filter(x, P):
 
 measurements = [1, 2, 3]
 
-x = matrix([[0.], [0.]]) # initial state (location and velocity)
+x = matrix([[0.], [0.]])  # initial state (location and velocity)
 P = matrix([[1000., 0.], [0., 1000.]]) # initial uncertainty
 u = matrix([[0.], [0.]]) # external motion
 F = matrix([[1., 1.], [0, 1.]]) # next state function
